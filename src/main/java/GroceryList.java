0f27aea426aab7e08e9126a0d9bed9591937cc65
dbond <debon1426@gmail.com>
2020-03-23 14:00:57 -0400

    commmmmmiting!

---
+++ src/main/java/GroceryList.java
@@ -0,0 +1,46 @@
+public class GroceryList {
+
+    String name;
+    String price;
+    String food;
+    String expiration;
+
+    public GroceryList(String name, String price, String food, String expiration) {
+        this.name = name;
+        this.price = price;
+        this.food = food;
+        this.expiration = expiration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(String expiration) {
+        this.expiration = expiration;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import com.google.common.base.Splitter;
+import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -9,9 +15,101 @@ public class Main {
         return result;
     }
 
+    public String [] putInArray (String result){
+       String [] strArray = result.split("[##;%^@!*]");
+            for (String s: strArray){
+                System.out.println(s);
+            }
+        return strArray;
+    }
+
+
+
+   public ArrayList<GroceryList> itemsToGroceries(String[] strArray){
+       ArrayList<GroceryList>  list = new ArrayList<GroceryList>();
+       String name = "";
+       String price = "";
+       String food = "";
+       String expiration= "";
+       GroceryList groceryList;
+       for ( int j=0; j<strArray.length;j++){
+            strArray[j] = strArray[j].toLowerCase();
+
+            if (j%5==0) {
+                groceryList = new GroceryList(name, price, food, expiration);
+                list.add(groceryList);
+                 name = "";
+                 price = "";
+                 food = "";
+                 expiration= "";
+
+            }
+            //          map= Splitter.on(" ").withKeyValueSeparator(":").split(s);
+            String[] split = strArray[j].split(":");
+            for (int i = 0; i < split.length - 1; i++) {
+
+                if (split[i].equals("name")) {
+                    name = split[i + 1];
+                }
+                if (split[i].equals("price")) {
+                    price = split[i + 1];
+                }
+                if (split[i].equals("type")) {
+                    food = split[i + 1];
+                }
+                if (split[i].equals("expiration")) {
+                    expiration = split[i + 1];
+
+                }
+             }
+         }return list;
+    }
+
+    public void putItemInMapForName(ArrayList<GroceryList> list){
+        Map nameMap = new HashMap<String,Integer>();
+
+        Map priceMap = new HashMap<String,Integer>();
+
+
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<String> price = new ArrayList<>();
+
+
+        for(GroceryList g :list) {
+            name.add(g.getName());
+            price.add(g.getPrice());
+        }
+
+        for (String s: name){
+            int num=Collections.frequency(name,s);
+            nameMap.put(s,num);
+        }
+        for (String s: price){
+            int num=Collections.frequency(price,s);
+            priceMap.put(s,num);
+        }
+        System.out.println(nameMap);
+        System.out.println(priceMap);
+
+    }
+
+
+
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
+
+        String [] arr = (new Main().putInArray(output));
+
+        ArrayList s= (new Main()).itemsToGroceries(arr);
+
+        (new Main()).putItemInMapForName(s);
+
+
+
+
+
+
 
     }
 }
