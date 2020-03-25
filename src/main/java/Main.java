8719f9d66ef66570fc468c5f4f73d21f0fa08908
dbond <debon1426@gmail.com>
2020-03-25 19:52:47 -0400

    added map<string,arraylist>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,41 @@
-import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
 import org.apache.commons.io.IOUtils;
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.time.LocalDate;
+
 import java.util.*;
-import java.util.regex.Pattern;
 
 public class Main {
 
-    public String readRawDataToString() throws Exception{
+    public String readRawDataToString() throws Exception {
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
 
-    public String [] putInArray (String result){
-       String [] strArray = result.split("[##;%^@!*]");
-            for (String s: strArray){
-                System.out.println(s);
-            }
+    public String[] putInArray(String result) {
+        String[] strArray = result.split("[##;%^@!*]");
+        for (String s : strArray) {
+            System.out.println(s);
+        }
         return strArray;
     }
 
 
-
-   public ArrayList<GroceryList> itemsToGroceries(String[] strArray){
-       ArrayList<GroceryList>  list = new ArrayList();
-       String name = "";
-       String price = "";
-       String food = "";
-       String expiration= "";
-       GroceryList groceryList;
-       for ( int j=0; j<strArray.length;j++){
+    public ArrayList<GroceryList> itemsToGroceries(String[] strArray) {
+        ArrayList<GroceryList> list = new ArrayList();
+        String name = "";
+        String price = "";
+        String food = "";
+        String expiration = "";
+        GroceryList groceryList;
+        for (int j = 0; j < strArray.length; j++) {
             strArray[j] = strArray[j].toLowerCase();
 
-            if (j%5==0) {
+            if (j % 5 == 0) {
                 groceryList = new GroceryList(name, price, food, expiration);
                 list.add(groceryList);
-                 name = "";
-                 price = "";
-                 food = "";
-                 expiration= "";
+                name = "";
+                price = "";
+                food = "";
+                expiration = "";
 
             }
             //          map= Splitter.on(" ").withKeyValueSeparator(":").split(s);
@@ -48,7 +43,7 @@ public class Main {
             for (int i = 0; i < split.length - 1; i++) {
 
                 if (split[i].equals("name")) {
-                    split[i+1]=split[i+1].replaceAll("[0-9]","o");
+                    split[i + 1] = split[i + 1].replaceAll("[0-9]", "o");
                     name = split[i + 1];
                 }
                 if (split[i].equals("price")) {
@@ -61,41 +56,37 @@ public class Main {
                     expiration = split[i + 1];
 
                 }
-             }
-         }return list;
+            }
+        }
+        return list;
     }
 
-    public void putItemInMapForName(ArrayList<GroceryList> list){
-        Map nameMap = new HashMap<String,Integer>();
+    public void putItemInMapForName(ArrayList<GroceryList> list) {
+        Map nameMap = new HashMap<String, Integer>();
 
-        Map priceMap = new HashMap<String,Integer>();
-        Map<String,ArrayList<String>> myMap = new HashMap();
-
+        Map priceMap = new HashMap<String, Integer>();
+        Map<String, ArrayList<String>> myMap = new HashMap();
 
 
         ArrayList<String> name = new ArrayList<>();
         ArrayList<String> price = new ArrayList<>();
 
 
-        for(GroceryList g :list) {
-
+        for (GroceryList g : list) {
             name.add(g.getName());
             price.add(g.getPrice());
-            myMap.put(g.getName(),price);
-        }
-        for (String k: name) {
-            int num1 = Collections.frequency(name, k);
-            nameMap.put(k, num1);
-
-            for (String s : price) {
-                int num2 = Collections.frequency(price, s);
-                priceMap.put(s, num2);
 
 
-            }
         }
 
-
+        for (String h : name) {
+            int num = Collections.frequency(name, h);
+            nameMap.put(h, num);
+        }
+        for (String s : price) {
+            int num2 = Collections.frequency(price, s);
+            priceMap.put(s, num2);
+        }
 
 
         System.out.println(nameMap);
@@ -104,6 +95,31 @@ public class Main {
 
 
     }
+
+    public void pricePerItem(ArrayList<GroceryList> list){
+        Map map = new HashMap();
+        List array = new ArrayList();
+        for(GroceryList g : list) {
+            for (int i = 0; i < list.size(); i++) {
+
+                if (g.getName().equals(list.get(i).getName())) {
+                    array.add(g.getPrice());
+                }
+                }if (!map.containsKey(g.getName())) {
+                map.put(g.getName(),array);
+            }      array= new ArrayList();
+
+
+
+
+        }
+        System.out.println(map);
+
+
+
+
+    }
+
 
 
 
@@ -115,13 +131,9 @@ public class Main {
 
         ArrayList s= (new Main()).itemsToGroceries(arr);
 
+        (new Main()).pricePerItem(s);
         (new Main()).putItemInMapForName(s);
 
 
-
-
-
-
-
-    }
+}
 }
